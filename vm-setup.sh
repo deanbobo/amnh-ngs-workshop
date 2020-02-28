1a3d8e40b3d70a579ce26b3d3e3ad073208df509
AMNH NGS Workshop <deanbobo@gmail.com>
2020-02-28 08:04:38 -0500

    turns off screen lock

---
+++ vm-setup.sh
@@ -0,0 +1,7 @@
+
+#run this script to set up environmental variables. We'll also put code in here to fix issues with the virtual machine
+
+#turn off lock screen
+gsettings set org.gnome.desktop.lockdown disable-lock-screen 'true'
+
+
