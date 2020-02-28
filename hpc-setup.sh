56b4b0824360b2deda1d5c9d6786dc1ecb8547fa
Dean Bobo <deanbobo@gmail.com>
2020-02-28 08:40:50 -0500

    Update and rename setup.sh to hpc-setup.sh

--- hpc-setup.sh
+++ hpc-setup.sh
@@ -1,2 +1,4 @@
 #run this script to set up environmental variables. We'll also put code in here to fix issues with the virtual machine.
 
+#load these modules
+module load bwa-0.7.15 samtools-1.5 fastqc-0.11.5 picard-2.10.5 bedtools-2.26.0
